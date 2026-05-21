5dfdff9d3bf1a9a82bae7fc026b9aba7a5f70d4a
Hong-Phuc Bui <hong-phuc.bui@htwsaar.de>
2026-05-21 02:00:11 +0200

    deploy to maven central, compile with java 21

--- deploy-sablecc-3.7-to-mavencentral.sh
+++ deploy-sablecc-3.7-to-mavencentral.sh
@@ -1,15 +1,4 @@
 #!/bin/sh
-DIST_LIB=sablecc-3.7
-set -x
-mvn source:jar
-mvn javadoc:javadoc; mvn javadoc:jar
-mvn gpg:sign-and-deploy-file \
-  -DpomFile=pom.xml \
-  -Dfile=${DIST_LIB}/lib/sablecc.jar\
-  -Dsources=./target/${DIST_LIB}-sources.jar\
-  -Djavadoc=./target/${DIST_LIB}-javadoc.jar\
-  -DrepositoryId=ossrh\
-  -Durl='https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/'
+# Doku + Anleitung zum Deployment: https://central.sonatype.org/pages/support/
 
-
-rm pom.xml.asc
+mvn -P'-nexus,central.sonatype.com' clean deploy
